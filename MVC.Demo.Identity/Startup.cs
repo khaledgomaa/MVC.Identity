214f363acf8f23de8a1c01eb80932118a1a2bd8a
Khaled Gomaa <khaledgomaa670@gmail.com>
2024-11-06 16:28:06 +0200

    init

---
+++ MVC.Demo.Identity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC.Demo.Identity.Startup))]
+namespace MVC.Demo.Identity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
